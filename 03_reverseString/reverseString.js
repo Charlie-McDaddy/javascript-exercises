e5b10e762b221c07744e75b4dfef3ab3c061c319
Charlie-McDaddy <coding.0awnx@aleeas.com>
2022-02-19 16:40:51 +1000

    completed reverseString exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function(str) {
+let charArr,
+  reverseArr,
+  joinArr = [];
 
+const reverseString = function (str) {
+  charArr = str.split("");
+  reverseArr = charArr.reverse();
+  joinArr = reverseArr.join("");
+  return joinArr;
 };
 
 // Do not edit below this line
