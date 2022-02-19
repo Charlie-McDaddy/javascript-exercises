fda25ca05dabe0c08c2ef1cf59402d3dc4c2c232
Charlie-McDaddy <coding.0awnx@aleeas.com>
2022-02-19 16:14:10 +1000

    completed repeatString exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function (string, number) {
+  let repString = "";
+  if (number < 0) {
+    return "ERROR";
+  }
+  for (let i = 1; i <= number; i++) {
+    repString += string;
+  }
+  return repString;
 };
 
 // Do not edit below this line
